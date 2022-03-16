8c1d2e35bd204f02b3d6260dccf1c03c586ff779
Alperen Kapusuz <sefalperen21@gmail.com>
2022-03-16 21:58:01 +0300

    contact tamamlandı

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,17 +1,48 @@
-import React from 'react'
-import './Contact.css'
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <div className="contact">
-        <h2>İletişim</h2>
+      <h2 className="contact__heading">İLETİŞİM</h2>
+      <div className="contact__item">
         <p>Cep : 0 553 151 04 85</p>
         <p>E-posta : sefalperen21@gmail.com</p>
         <p>Adres : İstanbul / Pendik</p>
-        <p><a href="https://www.linkedin.com/in/alperen-kapusuz-a72a381a5/">LinkedIN</a></p>
-        <p><a href="https://alperenkapusuz.github.io/">Web Sayfam</a></p> 
+        <br />
+        <a href="https://twitter.com/alprnkpsz" target="blank">
+          <img
+            align="center"
+            src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg"
+            alt="alprnkpsz"
+            height="30"
+            width="40"
+          />
+        </a>
+        <a
+          href="https://linkedin.com/in/alperen-kapusuz-a72a381a5"
+          target="blank"
+        >
+          <img
+            align="center"
+            src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg"
+            alt="alperen-kapusuz-a72a381a5"
+            height="30"
+            width="40"
+          />
+        </a>
+        <a href="https://stackoverflow.com/users/15199653" target="blank">
+          <img
+            align="center"
+            src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/stack-overflow.svg"
+            alt="15199653"
+            height="30"
+            width="40"
+          />
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
